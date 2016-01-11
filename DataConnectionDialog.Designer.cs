2945e04098f35d75dcb29c3789c25705438ae4f3
Remus Rusanu <contact@rusanu.com>
2016-01-11 04:23:22 -0800

    Initial Release

---
+++ DataConnectionDialog.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace com.rusanu.dataconnectiondialog
+{
+    partial class DataConnectionDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.serverName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.authenticationMode = new System.Windows.Forms.ComboBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.userPassword = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server name:";
+            // 
+            // serverName
+            // 
+            this.serverName.Location = new System.Drawing.Point(151, 25);
+            this.serverName.Name = "serverName";
+            this.serverName.Size = new System.Drawing.Size(248, 20);
+            this.serverName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Authentication:";
+            // 
+            // authenticationMode
+            // 
+            this.authenticationMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.authenticationMode.FormattingEnabled = true;
+            this.authenticationMode.Location = new System.Drawing.Point(151, 66);
+            this.authenticationMode.Name = "authenticationMode";
+            this.authenticationMode.Size = new System.Drawing.Size(248, 21);
+            this.authenticationMode.TabIndex = 3;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(62, 112);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(64, 13);
+            this.lblUserName.TabIndex = 4;
+            this.lblUserName.Text = "User  name:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(62, 153);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Password:";
+            // 
+            // userName
+            // 
+            this.userName.Location = new System.Drawing.Point(185, 108);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(214, 20);
+            this.userName.TabIndex = 6;
+            // 
+            // userPassword
+            // 
+            this.userPassword.Location = new System.Drawing.Point(185, 149);
+            this.userPassword.Name = "userPassword";
+            this.userPassword.Size = new System.Drawing.Size(214, 20);
+            this.userPassword.TabIndex = 7;
+            this.userPassword.UseSystemPasswordChar = true;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnConnect.Location = new System.Drawing.Point(241, 188);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(324, 188);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(28, 188);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 10;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoEllipsis = true;
+            this.lblStatus.Location = new System.Drawing.Point(28, 228);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(371, 63);
+            this.lblStatus.TabIndex = 11;
+            // 
+            // DataConnectionDialog
+            // 
+            this.AcceptButton = this.btnConnect;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(435, 300);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.userPassword);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.authenticationMode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.serverName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DataConnectionDialog";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Connect to SQL Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox serverName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox authenticationMode;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.TextBox userPassword;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
---
+++ DataConnectionDialog.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Principal;
+
+namespace com.rusanu.dataconnectiondialog
+{
+    /// <summary>
+    /// SQL Server connection dialog
+    /// </summary>
+    public partial class DataConnectionDialog : Form
+    {
+        /// <summary>
+        /// The connection string builder
+        /// </summary>
+        public SqlConnectionStringBuilder ConnectionStringBuilder { get; private set; }
+
+        /// <summary>
+        /// Internal class for form/connection properties
+        /// </summary>
+        internal class Properties : INotifyPropertyChanged
+        {
+            /// <summary>
+            /// Strings used in code
+            /// </summary>
+            internal class Tags
+            {
+                public static readonly string DataSource = "DataSource";
+                public static readonly string UserName = "UserName";
+                public static readonly string Password = "Password";
+                public static readonly string IntegratedSecurity = "IntegratedSecurity";
+                public static readonly string DataSourceValid = "DataSourceValid";
+                public static readonly string UserNameEnabled = "UserNameEnabled";
+                public static readonly string WindowsAuthentication = "Windows Authentication";
+                public static readonly string SQLServerAuthentication = "SQL Server Authentication";
+                public static readonly string AuthenticationMode = "AuthenticationMode";
+                public static readonly string TestingEnabled = "TestingEnabled";
+                public static readonly string TestResult = "TestResult";
+            };
+
+            /// <summary>
+            /// From INotifyPropertyChanged
+            /// </summary>
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            /// <summary>
+            /// The current connection string builder
+            /// </summary>
+            public SqlConnectionStringBuilder ConnectionStringBuilder { get; private set; }
+
+            /// <summary>
+            /// ConnectionStreingBuilder.DataSource + change notifications
+            /// </summary>
+            public string DataSource
+            {
+                get { return ConnectionStringBuilder.DataSource; }
+                set
+                {
+                    ConnectionStringBuilder.DataSource = value;
+                    NotifyPropertyChanged(Tags.DataSource);
+                    NotifyPropertyChanged(Tags.DataSourceValid);
+                    NotifyPropertyChanged(Tags.TestingEnabled);
+                    TestResult = String.Empty;
+                }
+            }
+
+            /// <summary>
+            /// True when the data source is not empty (for Enabled data binding)
+            /// </summary>
+            public bool DataSourceValid
+            {
+                get { return !string.IsNullOrEmpty(DataSource); }
+            }
+
+            /// <summary>
+            /// ConnectionStringBuilder.IntegratedSecurity + change notifications
+            /// </summary>
+            public bool IntegratedSecurity
+            {
+                get { return ConnectionStringBuilder.IntegratedSecurity; }
+                set
+                {
+                    if (value != ConnectionStringBuilder.IntegratedSecurity)
+                    {
+                        ConnectionStringBuilder.IntegratedSecurity = value;
+                        NotifyPropertyChanged(Tags.IntegratedSecurity);
+                        NotifyPropertyChanged(Tags.UserNameEnabled);
+                        NotifyPropertyChanged(Tags.AuthenticationMode);
+                        TestResult = String.Empty;
+                    }
+                }
+            }
+
+            /// <summary>
+            /// ConnectionStringBuilder.UserID + change notifications
+            /// For IntegratedSecurity it returns current Windows identity
+            /// </summary>
+            public string UserName
+            {
+                get { return IntegratedSecurity ? WindowsUserName : ConnectionStringBuilder.UserID; }
+                set
+                {
+                    ConnectionStringBuilder.UserID = value;
+                    NotifyPropertyChanged(Tags.UserName);
+                    TestResult = String.Empty;
+                }
+            }
+
+            /// <summary>
+            /// ConnectionStringBuilder.Password + change notifications
+            /// For IntegratedSecurity will return ""
+            /// </summary>
+            public string Password
+            {
+                get { return IntegratedSecurity ? String.Empty : ConnectionStringBuilder.Password; }
+                set
+                {
+                    ConnectionStringBuilder.Password = value;
+                    NotifyPropertyChanged(Tags.Password);
+                    TestResult = String.Empty;
+                }
+            }
+
+            /// <summary>
+            /// True when not ConnectionStringBuilder.IntegratedSecurity (for Enabled binding)
+            /// </summary>
+            public bool UserNameEnabled
+            {
+                get { return !IntegratedSecurity; }
+            }
+
+            /// <summary>
+            /// ConnectionStringBuilder.IntegratedSecurity mapped to values for the combo data binding
+            /// </summary>
+            public string AuthenticationMode
+            {
+                get { return IntegratedSecurity ? Tags.WindowsAuthentication : Tags.SQLServerAuthentication; }
+                set
+                {
+                    IntegratedSecurity = (value == Tags.WindowsAuthentication);
+                }
+            }
+
+            private bool _isTesting = false;
+
+            /// <summary>
+            /// Set to true when the connection is being tested
+            /// </summary>
+            public bool IsTesting 
+            { 
+                get { return _isTesting; }
+                set
+                {
+                    _isTesting = value;
+                    NotifyPropertyChanged(Tags.TestingEnabled);
+                }
+            }
+
+            /// <summary>
+            /// True when Testing is allowed (for Rnabled binding)
+            /// </summary>
+            public bool TestingEnabled
+            {
+                get { return DataSourceValid && !IsTesting; }
+            }
+
+            private string _testResult = "";
+
+            /// <summary>
+            /// Connection test result
+            /// </summary>
+            public string TestResult
+            {
+                get {return _testResult;}
+                set
+                {
+                    _testResult = value;
+                    NotifyPropertyChanged(Tags.TestResult);
+                }
+            }
+
+            /// <summary>
+            /// The current Windows user, cached
+            /// </summary>
+            internal string WindowsUserName { get; set; }
+
+            /// <summary>
+            /// Notification for INotifyPropertyChanged
+            /// </summary>
+            /// <param name="propertyName"></param>
+            internal void NotifyPropertyChanged(string propertyName)
+            {
+                PropertyChangedEventHandler handler = PropertyChanged;
+                if (handler != null)
+                {
+                    handler(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+
+            /// <summary>
+            /// CTOR
+            /// </summary>
+            /// <param name="scsb">Preconfigured SqlConnectionStringBuilder</param>
+            public Properties(SqlConnectionStringBuilder scsb)
+            {
+                ConnectionStringBuilder = scsb;
+                WindowsUserName = WindowsIdentity.GetCurrent().Name;
+            }
+        }
+
+        /// <summary>
+        /// The current properties
+        /// </summary>
+        private Properties FormProperties { get; set; }
+
+        /// <summary>
+        /// CTOR
+        /// </summary>
+        /// <param name="connectionStringBuilder">The preconfigured connection string builder</param>
+        public DataConnectionDialog(SqlConnectionStringBuilder connectionStringBuilder)
+        {
+            InitializeComponent();
+
+            authenticationMode.Items.Add(Properties.Tags.WindowsAuthentication);
+            authenticationMode.Items.Add(Properties.Tags.SQLServerAuthentication);
+
+            ConnectionStringBuilder = connectionStringBuilder ?? new SqlConnectionStringBuilder()
+                {
+                    IntegratedSecurity = true,
+                    DataSource = "."
+                };
+
+            FormProperties = new Properties(ConnectionStringBuilder);
+
+            serverName.DataBindings.Add("Text", FormProperties, Properties.Tags.DataSource, false, DataSourceUpdateMode.OnPropertyChanged);
+            userName.DataBindings.Add("Text", FormProperties, Properties.Tags.UserName);
+            userPassword.DataBindings.Add("Text", FormProperties, Properties.Tags.Password);
+
+            authenticationMode.DataBindings.Add("Text", FormProperties, Properties.Tags.AuthenticationMode, false, DataSourceUpdateMode.OnPropertyChanged);
+
+            userName.DataBindings.Add("Enabled", FormProperties, Properties.Tags.UserNameEnabled);
+            userPassword.DataBindings.Add("Enabled", FormProperties, Properties.Tags.UserNameEnabled);
+            lblUserName.DataBindings.Add("Enabled", FormProperties, Properties.Tags.UserNameEnabled);
+            lblPassword.DataBindings.Add("Enabled", FormProperties, Properties.Tags.UserNameEnabled);
+
+            btnConnect.DataBindings.Add("Enabled", FormProperties, Properties.Tags.DataSourceValid);
+            btnTest.DataBindings.Add("Enabled", FormProperties, Properties.Tags.TestingEnabled);
+
+            lblStatus.DataBindings.Add("Text", FormProperties, Properties.Tags.TestResult);
+        }
+
+        internal void TestConnection()
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionStringBuilder.ConnectionString))
+            {
+                try
+                {
+                    FormProperties.IsTesting = true;
+                    FormProperties.TestResult = "Testing...";
+                    conn.Open();
+                    FormProperties.TestResult = "Success!";
+                }
+                catch (Exception ex)
+                {
+                    FormProperties.TestResult = ex.Message;
+                }
+                finally
+                {
+                    FormProperties.IsTesting = false;
+                }
+            }
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            TestConnection();
+        }
+
+        
+    }
+}
